08b6ec4b3379f5c9b8c5513992e3be6ce0b41535
Tanh101 <lytanh@starboard-asia.com>
2023-05-11 17:28:00 +0700

    create order model

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -7,19 +7,89 @@ const OrderSchema = new Schema({
         ref: 'user',
         required: true
     },
+    shipper: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
     paymentMethod: {
         type: String,
         required: true,
         enum: ['cash', 'card']
     },
-    restaurant: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'restaurant',
+    orderStatus: {
+        type: String,
+        required: true,
+        default: 'preparing',
+        enum: ['accepted', 'preparing', 'ready', 'delivered', 'delivering', 'cancelled']
+    },
+    paymentStatus: {
+        type: String,
+        required: true,
+        default: 'unpaid',
+        enum: ['unpaid', 'paid']
+    },
+    orderItems: [
+        {
+            product: {
+                name: {
+                    type: String,
+                    required: true
+                },
+                price: {
+                    type: Number,
+                    required: true
+                },
+                description: {
+                    type: String,
+                    required: true
+                },
+                media: [{
+                    type: {
+                        type: String,
+                        required: true,
+                        enum: ['image', 'video']
+                    },
+                    url: {
+                        type: String,
+                        required: true
+                    }
+                }],
+                category: {
+                    type: String,
+                    required: true,
+                },
+                restaurant: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'restaurant',
+                    required: true
+                },
+                status: {
+                    type: String,
+                    required: true,
+                    default: 'active',
+                    enum: ['active', 'deleted', 'deactive']
+                }
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    total: {
+        type: Number,
         required: true
     },
-
-
-}, {timestamps: true});
+    note: {
+        type: String,
+        default: null
+    },
+    deleteAt: {
+        type: Date,
+        default: null
+    }
+}, { timestamps: true });
 
 
 module.exports = mongoose.model('order', OrderSchema);
