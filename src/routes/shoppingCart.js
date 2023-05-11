781a168ac4f643ed9b00db63d968b665e973f30e
Tanh101 <lytanh@starboard-asia.com>
2023-05-11 14:35:25 +0700

    create api shopping cart, get cart by user and sort items by create time

---
+++ src/routes/shoppingCart.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const shoppingCartController = require('../app/controllers/shoppingCartController');
+const auth = require('../middleware/auth');
+
+// @route GET /cart
+// @desc Get cart by user id
+// @access private: current user
+router.get('/', auth.verifyToken, auth.checkRole("user"), shoppingCartController.getCart);
+
+// @route POST /cart
+// @desc Add item to cart
+// @access private: current user
+router.post('/', auth.verifyToken, auth.checkRole('user'), shoppingCartController.addToCart);
+
+// @route PUT /cart
+// @desc remove item from cart
+// @access private: current user
+router.put('/remove', auth.verifyToken, auth.checkRole('user'), shoppingCartController.removeCart);
+
+// @route PUT /cart
+// @desc Update item from cart
+// @access private: current user
+router.put('/', auth.verifyToken, auth.checkRole('user'), shoppingCartController.updateCart);
+
+// @route DELETE /cart
+// @desc Delete item from cart
+// @access private: current user
+router.delete('/', auth.verifyToken, auth.checkRole('user'), shoppingCartController.deleteCart);
+
+
+
+module.exports = router;
